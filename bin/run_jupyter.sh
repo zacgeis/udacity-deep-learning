b44e06894e33217baf019ae9cff8c99390dfbdbc
Zach Geis <zacgeis@gmail.com>
2017-05-12 17:00:26 +0000

    Update run command

--- bin/run_jupyter.sh
+++ bin/run_jupyter.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-jupyter notebook "$@" --certfile=/jupyter.pem --keyfile /jupyter.key --port=443 --ip=0.0.0.0
+jupyter notebook "$@" --certfile=./jupyter.pem --keyfile ./jupyter.key --port=443 --ip=0.0.0.0
 
