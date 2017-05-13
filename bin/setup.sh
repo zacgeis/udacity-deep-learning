05c8b729b53629d6ed90735853deedd144ad592e
Zach Geis <zacgeis@gmail.com>
2017-05-13 00:52:52 +0000

    Project 5 update

--- bin/setup.sh
+++ bin/setup.sh
@@ -13,3 +13,4 @@ apt-get install python3-pip -y
 pip3 install --upgrade pip
 pip3 install tensorflow-gpu
 pip3 install jupyter
+echo "you also have to install https://developer.nvidia.com/compute/machine-learning/cudnn/secure/v6/prod/8.0_20170427/Ubuntu16_04_x64/libcudnn6_6.0.21-1+cuda8.0_amd64-deb"
